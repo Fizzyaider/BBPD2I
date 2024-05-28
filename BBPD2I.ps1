d96f1eb7f1c672a46bc43f3a0ec655c240804a83
Fizzyaider <70167087+Fizzyaider@users.noreply.github.com>
2024-05-28 17:06:31 +0100

    v1.2-beta.1.
    This iteration of the BBPD2I script comes with the auto game installation location detection feature, this build however is in beta and a new full release will not happen now until more features are added on that warrant a full new main release

--- BBPD2I.ps1
+++ BBPD2I.ps1
@@ -1,25 +1,58 @@
-# BBPD2I (Basic Bitch Payday 2 Installer) v1.1 By Fizzyaider
+# BBPD2I (Basic Bitch Payday 2 Installer) v1.2[BETA] By Fizzyaider
 # I skipped dinner to make the original script lol
 
+# Variable declaration section here essentially
 $RARDirectory = $PSScriptRoot
-$PDDrive = Read-Host "Please enter the letter of the drive that Payday 2 is installed on [PLEASE ENSURE THAT STEAM IS INSTALLED AT THE ROOT OF THE DRIVE]"
+
+$drives = Get-PSDrive -PSProvider FileSystem
+
+$PDDrive = ""
+
+echo 'Drives detected, attempting to automatically find payday 2 installation'
+
+# What this for loop will do is go through all of the drives that are found within the system and then proceed to 
+for ($DIndex = 0; $DIndex -lt $drives.count; $DIndex++)
+{
+    if ([string]$drives[$DIndex] -eq "C")
+    {
+        if ($(Test-Path -Path "$([string]$drives[$DIndex]):\Program Files (x86)\Steam\steamapps\common\PAYDAY 2") -eq "True")
+        {
+            echo "Payday 2 installation found! Drive: $([string]$drives[$DIndex])"
+            $PDDrive = $([string]$drives[$DIndex])
+            break
+        }
+        
+    } else {
+        if ($(Test-Path -Path "$([string]$drives[$DIndex]):\SteamLibrary\steamapps\common\PAYDAY 2") -eq "True")
+        {
+            echo "Payday 2 installation found! Drive: $([string]$drives[$DIndex])"
+            $PDDrive = $([string]$drives[$DIndex])
+            break
+        }
+    }
+}
+
 $PI = "$($PDDrive):\SteamLibrary\steamapps\common\PAYDAY 2\mods"
 $PDMD = "$($PDDrive):\SteamLibrary\steamapps\common\PAYDAY 2"
 [int]$score = 0
-echo $RARDirectory > "./log.txt"
 $MODNames = ".\tmp\BeardLib.zip",".\tmp\vanhudplus.zip",".\tmp\WD2MainMenu.zip"
 $OKFiles = "base","downloads","logs","saves"
 
 
+# In this section - BBPD2I is searching to find if "WSOCK2.dll" is present on the payday 2 installation folder, if it is not
+# then what will happen is that it will send a download web request to the URL that contains the WSOCK2.dll file
 if ($(Test-Path "$($PDDrive):\SteamLibrary\steamapps\common\PAYDAY 2\WSOCK32.dll") -eq $false)
 {
     echo "SuperBLT library file not found, downloading to payday 2 installation . . ."
     Invoke-WebRequest "https://sblt-update.znix.xyz/pd2update/download/get.php?src=homepage&id=payday2bltwsockdll" -OutFile "$($PDMD)\WSOCK32.dll"
 }
 
+# This little bit here copies all of the folders within the payday 2 installation 'mod' folder if they are present and then place them into a
+# '.tmp' folder within the directory that the installation script is running in. This serves as a backup in the case that BBPD2I does not
+# work properly.
 for ($CIndex = 0; $CIndex -lt $OKFiles.count; $CIndex++)
 {
-	Write-Host (-join("$PI","\","$($OKFiles[$CIndex])"))
+	# Write-Host (-join("$PI","\","$($OKFiles[$CIndex])"))
 	Copy-Item -Path "$(-join("$PI","\","$($OKFiles[$CIndex])"))" -Destination ".\tmp" -Recurse
 }
 
